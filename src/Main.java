9e5421002a83913876ae5f7cf6c3d0dcc9891736
Henrique Pimentel <henrique.pimentel11@gmail.com>
2026-07-15 14:14:20 -0300

    Banco conectado com o app

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,16 @@
+import connection.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.SQLException;
 public class Main {
-    
+    public static void main(String[] args) throws SQLException {
+
+        Connection conexao = ConnectionFactory.conectar();
+
+        if (conexao != null) {
+            System.out.println("CONECTADO COM SUCESSO!");
+        } else {
+            System.out.println("DEU RUIM!");
+        }
+
+    }
 }
--- src/connection/ConnectionFactory.java
+++ src/connection/ConnectionFactory.java
@@ -1,5 +1,27 @@
 package connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
-    
+    private static final String enderecoIP = "localhost";
+    private static final String enderecoPorta = "5432";
+    private static final String username = "postgres";
+    private static final String password = "admin";
+    private static final String nomeBanco = "postgres";
+
+
+    public static Connection conectar() {
+
+        try {
+            return DriverManager.getConnection(
+                    "jdbc:postgresql://"+enderecoIP+":"+enderecoPorta+"/"+nomeBanco,
+                    username,
+                    password
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
