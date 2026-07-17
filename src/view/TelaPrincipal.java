ce99279661a0856b03b70c73150ef665a28a8ae6
Henrique Pimentel <henrique.pimentel11@gmail.com>
2026-07-17 19:28:17 -0300

    Update TelaPrincipal.java

--- src/view/TelaPrincipal.java
+++ src/view/TelaPrincipal.java
@@ -1,5 +1,13 @@
 package view;
 
+import connection.ConnectionFactory;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
 public class TelaPrincipal {
     
 }
